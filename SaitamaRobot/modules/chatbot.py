94ba196d83d4c68ed1a0b24008cdc1620aa6049c
GAMERFUCKER <bsdkbhaag248@gmail.com>
2021-01-21 22:38:47 +0530

    Update chatbot.py

--- SaitamaRobot/modules/chatbot.py
+++ SaitamaRobot/modules/chatbot.py
@@ -136,7 +136,7 @@ def list_chatbot_chats(update: Update, context: CallbackContext):
 
 
 __help__ = f"""
-Chatbot utilizes the CoffeeHouse API and allows Saitama to talk and provides a more interactive group chat experience.
+Chatbot utilizes the CoffeeHouse API and allows Miakhalifa to talk and provides a more interactive group chat experience.
 
 *Commands:* 
 *Admins only:*
